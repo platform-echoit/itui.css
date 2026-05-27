dd30344cc69576ddccf9931e3b186c4eb29044d9
Nam <vannam1329@gmail.com>
2026-05-27 08:48:25 +0700

    feat: map file type

--- src/components/file-type/index.ts
+++ src/components/file-type/index.ts
@@ -1,5 +1,5 @@
 export { FileType } from './FileType';
 export type { FileTypeProps, FileTypeLogo, FileTypeVariant } from './FileType';
-export { FileIcon } from './FileIcon';
+export { FileIcon, getMimeExtension } from './FileIcon';
 export type { FileIconProps } from './FileIcon';
 export * from '../../icons/file-type';
