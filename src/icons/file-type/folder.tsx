d8cefaf7e16da93ccbe444c73720d9b335a04294
Quoc Tung <tung-nq@andvina.com>
2026-06-02 16:57:52 +0700

    Fix build

--- src/icons/file-type/folder.tsx
+++ src/icons/file-type/folder.tsx
@@ -1,5 +1,19 @@
 import FolderColorLogo from '../ITUI/file-type/FolderColorLogo';
 
-export function FolderIcon({ className, width, height }: { className?: string; width?: number; height?: number }) {
-  return <FolderColorLogo className={className} width={width ?? 20} height={height ?? 20} />;
+export function FolderIcon({
+  className,
+  width,
+  height,
+}: {
+  className?: string;
+  width?: number;
+  height?: number;
+}) {
+  return (
+    <FolderColorLogo
+      className={className}
+      width={width ?? 20}
+      height={height ?? 20}
+    />
+  );
 }
